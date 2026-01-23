9eee96ac1d2f08b974a563b32064cdcbf25ee981
devback90 <devback90.nexsol@gmail.com>
2026-01-23 13:30:00 +0900

    feat: enums

---
+++ core/core-enum/src/main/java/com/nexsol/tpa/core/enums/ContractStatus.java
@@ -0,0 +1,15 @@
+package com.nexsol.tpa.core.enums;
+
+public enum ContractStatus {
+
+    PENDING("가입대기"), COMPLETED("가입완료"), // UI의 '가입완료'
+    CANCELED("해지"), // UI의 '해지'
+    EXPIRED("기간만료"), ERROR("오류");
+
+    private final String description;
+
+    ContractStatus(String description) {
+        this.description = description;
+    }
+
+}
