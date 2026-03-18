f59fc0fcb89a7333cd218e51959d8ae1f6867399
devback90 <devback90.nexsol@gmail.com>
2026-03-18 13:32:30 +0900

    add to silsonExclude and family

--- storage/db-core/src/main/java/com/nexsol/tpa/storage/db/core/TravelInsurancePlanFamilyMapEntity.java
+++ storage/db-core/src/main/java/com/nexsol/tpa/storage/db/core/TravelInsurancePlanFamilyMapEntity.java
@@ -1,22 +1,29 @@
 package com.nexsol.tpa.storage.db.core;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "travel_insurance_plan_family_map")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class TravelInsurancePlanFamilyMapEntity extends BaseEntity {
+public class TravelInsurancePlanFamilyMapEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@Column(name = "family_id")
 	private Long familyId;
 
 	@Column(name = "plan_id")
 	private Long planId;
+
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
 
 }
